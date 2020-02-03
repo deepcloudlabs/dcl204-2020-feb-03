97ffc93ddc90d64d644b242aa9343dbed19ab654
deepcloudlabs <34dmng65jm>
2020-02-03 16:39:28 +0300

    format classes

--- study-primitive-types/src/com/example/StudyChar.java
+++ study-primitive-types/src/com/example/StudyChar.java
@@ -1,15 +1,20 @@
 package com.example;
 
+/**
+ * 
+ * @author Binnur Kurt <binnur.kurt@gmail.com>
+ *
+ */
 public class StudyChar {
 
 	public static void main(String[] args) {
 		char x = 'a'; // 2-byte, unicode
 		x++;
 		System.out.println(x);
-		System.out.println(x+0);
-        x = '\u20BA';
-        System.out.println(x);
-      
+		System.out.println(x + 0);
+		x = '\u20BA';
+		System.out.println(x);
+
 	}
 
 }
--- study-primitive-types/src/com/example/StudyString.java
+++ study-primitive-types/src/com/example/StudyString.java
@@ -1,16 +1,21 @@
 package com.example;
 
+/**
+ * 
+ * @author Binnur Kurt <binnur.kurt@gmail.com>
+ *
+ */
 public class StudyString {
 
 	public static void main(String[] args) {
 		String p = "Jack";
 		String q = new String("Jack");
 		String r = "Jack";
-		System.out.println("p==q: "+(p==q));
-		System.out.println("p.equals(q): "+p.equals(q));
-		System.out.println("p==r: "+(p==r));
-        q = q.intern();    
-		System.out.println("p==q: "+(p==q));
+		System.out.println("p==q: " + (p == q));
+		System.out.println("p.equals(q): " + p.equals(q));
+		System.out.println("p==r: " + (p == r));
+		q = q.intern();
+		System.out.println("p==q: " + (p == q));
 	}
 
 }
