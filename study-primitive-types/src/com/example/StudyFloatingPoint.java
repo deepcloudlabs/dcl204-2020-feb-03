a7d3bae95cc66566877e34cefee98d14d406214e
deepcloudlabs <34dmng65jm>
2020-02-03 16:38:55 +0300

    study floating point numbers

--- study-primitive-types/src/com/example/StudyFloatingPoint.java
+++ study-primitive-types/src/com/example/StudyFloatingPoint.java
@@ -2,6 +2,12 @@ package com.example;
 
 import java.math.BigDecimal;
 
+/**
+ * 
+ * @author Binnur Kurt <binnur.kurt@gmail.com>
+ *
+ */
+@SuppressWarnings("unused")
 public class StudyFloatingPoint {
 
 	public static void main(String[] args) {
@@ -10,18 +16,14 @@ public class StudyFloatingPoint {
 		double g = 3.1415; // 8-byte
 		float x = 1_000_000_000;
 		x += 50;
-		System.out.println(
-			String.format("%16.3f", x)
-		);
+		System.out.println(String.format("%16.3f", x));
 		double y = 2.0;
 		y = y - 1.10;
-		System.out.println(
-			String.format("%16.16f", y)
-		);
+		System.out.println(String.format("%16.16f", y));
 		BigDecimal bd = new BigDecimal("2.0");
 		bd = bd.subtract(new BigDecimal("1.1"));
 		System.out.println(bd);
-		double one = 0/0.;
+		double one = 0 / 0.;
 		System.out.println(Double.isNaN(one));
 	}
 
