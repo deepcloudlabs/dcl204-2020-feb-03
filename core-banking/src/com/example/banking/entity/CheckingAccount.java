0ecfa71c22ca3db419acba9ef7286cf3d665ccce
deepcloudlabs <deepcloudlabs@gmail.com>
2020-02-04 11:13:55 +0300

    implement CheckingAccount

---
+++ core-banking/src/com/example/banking/entity/CheckingAccount.java
@@ -0,0 +1,34 @@
+package com.example.banking.entity;
+
+/**
+ * 
+ * @author Binnur Kurt <binnur.kurt@gmail.com>
+ *
+ */
+public class CheckingAccount extends Account {
+	private double overdraftAmount;
+
+	public CheckingAccount(String iban, double balance, double overdraftAmount) {
+		super(iban, balance);
+		this.overdraftAmount = overdraftAmount;
+	}
+
+	public double getOverdraftAmount() {
+		return overdraftAmount;
+	}
+
+	public void setOverdraftAmount(double overdraftAmount) {
+		this.overdraftAmount = overdraftAmount;
+	}
+
+	@Override
+	public boolean withdraw(double amount) {
+		if (amount<=0.) return false;
+		if (amount>(balance+overdraftAmount))
+			return false;
+		this.balance -= amount;
+		return true;
+	}
+	
+	
+}
