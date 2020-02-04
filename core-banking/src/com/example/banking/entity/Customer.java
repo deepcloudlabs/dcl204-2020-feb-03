fb53668c78e9963206a37b7458fe6d0f1432e8c7
deepcloudlabs <deepcloudlabs@gmail.com>
2020-02-04 10:21:40 +0300

    functional implementation of getBalance() using Stream API

--- core-banking/src/com/example/banking/entity/Customer.java
+++ core-banking/src/com/example/banking/entity/Customer.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 /**
  * 
@@ -48,6 +49,27 @@ public class Customer {
 		return Optional.empty();
 	}
 
+	public double getBalance() {
+		double sum = 0.0;
+		for (Account acc : accounts)
+			sum += acc.getBalance();
+		return sum;
+	}
+
+	public double getBalance8() {
+//		final int x[]= {0};
+		return accounts.stream().parallel()
+//				.map(a -> {x[0]++; return a;})
+				.mapToDouble(Account::getBalance).sum();
+	}
+
+	public double getBalance_8() {
+		Stream<Account> stream = accounts.stream();
+		if (accounts.size() > 1_000_000)
+			stream = stream.parallel();
+		return stream.mapToDouble(Account::getBalance).sum();
+	}
+
 	public Account getAccount(int index) {
 		return accounts.get(index);
 	}
@@ -59,4 +81,5 @@ public class Customer {
 		}
 		return acc;
 	}
+
 }
